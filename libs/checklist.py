1d248e7c7d0ac16e8ca2d3d9599b65fd6c75667d
Kristaps Felzenbergs <kristaps@routedin.com>
2024-12-07 12:45:50 +0200

    feat: implement system users parsing and output

--- libs/checklist.py
+++ libs/checklist.py
@@ -1,14 +1,39 @@
+from datetime import datetime
+import json
 
 
 def check_configured_users(ssh):
     print("gathering configured users")
 
     # get who i am: username
-    command = "net user"
+    command = 'powershell -command "Get-LocalUser | ConvertTo-Json"'
     stdin, stdout, stderr = ssh.exec_command(command)
     stdin.close()
-    result = stdout.readlines()
-    print(result)
+    result = stdout.read()
+
+    account_list = []
+    if result:
+        result = json.loads(result)
+
+        for account in result:
+            # Enabled, Name, LastLogon
+
+            if account['LastLogon']:
+                timestamp_received = int(account['LastLogon'].split('(')[1].split(')')[0]) / 1000
+                lastLogon = str(datetime.fromtimestamp(timestamp_received))
+            else:
+                lastLogon = False
+
+            print(lastLogon)
+            obj_struct = {
+                "Enabled": account['Enabled'],
+                "LastLogon": lastLogon,
+                "Name": account['Name']
+            }
+
+            account_list.append(obj_struct)
+
+    return account_list
 
 
 def check_bitlocker_status(ssh):
@@ -54,17 +79,17 @@ def check_drive_status(ssh):
 def check_last_update_status(ssh):
     print("check when windows was last updated")
 
-    command = 'powershell -command Get-HotFix'
+    command = 'powershell -command "Get-HotFix  | ConvertTo-Json"'
     stdin, stdout, stderr = ssh.exec_command(command)
     stdin.close()
-    result = stdout.readlines()
-    print(result)
+    result = stdout.read()
+    return result
 
 
 def get_mpcomputerstatus(ssh):
     print("get various computer stats")
 
-    command = 'powershell -command "Get-MpComputerStatus"'
+    command = 'powershell -command "Get-MpComputerStatus | ConvertTo-Json"'
     stdin, stdout, stderr = ssh.exec_command(command)
     stdin.close()
     result = stdout.readlines()
